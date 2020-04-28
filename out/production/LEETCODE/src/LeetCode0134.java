f1947c8f227df7f843f2ea8091c975d9c61ede73
xiaoxin <940929347@qq.com>
2020-04-28 20:44:43 +0800

    LeetCode0134

---
+++ out/production/LEETCODE/src/LeetCode0134.java
@@ -0,0 +1,25 @@
+public class LeetCode0134 {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int res = -1;
+        int m = gas.length;
+        int[] two_gas = new int[2 * m];
+        int[] two_cost = new int[2 * m];
+        for (int i = 0; i < 2 * m; i++) {
+            two_gas[i] = gas[i % m];
+            two_cost[i] = cost[i % m];
+        }
+        f: for (int i = 0; i < m; i++) {
+            if (two_gas[i] < two_cost[i]) continue f;
+            else {
+                int presentOil = 0;
+                for (int j = i; j < i + m; j++) {
+                    presentOil += two_gas[j] - two_cost[j];
+                    if (presentOil < 0) break f;
+                }
+                return i;
+            }
+        }
+
+        return res;
+    }
+}
