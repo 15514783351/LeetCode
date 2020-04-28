b94d3d5d9a845364238059849ab5b446c4057ed4
xiaoxin <940929347@qq.com>
2020-04-28 20:11:23 +0800

    LeetCode0133

---
+++ src/LeetCode0133.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode0133 {
+    public Node cloneGraph(Node node) {
+        Map<Node, Node> lookup = new HashMap<>();
+        return dfs(node, lookup);
+    }
+
+    private Node dfs(Node node, Map<Node, Node> lookup) {
+        if (node == null) return null;
+        if (lookup.containsKey(node)) return lookup.get(node);
+        Node clone = new Node(node.val, new ArrayList<>());
+        lookup.put(node, clone);
+        for (Node n: node.neighbors) clone.neighbors.add(dfs(n, lookup));
+        return clone;
+    }
+}
