d794e1c506c87699b73744b1071145903349bb85
xiaoxin <940929347@qq.com>
2020-05-09 19:33:15 +0800

    二叉树的前序遍历

--- out/production/LEETCODE/src/LeetCode0144.java
+++ out/production/LEETCODE/src/LeetCode0144.java
@@ -1,13 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class LeetCode0144 {
     public List<Integer> output = new ArrayList<>();
+//    public List<Integer> preorderTraversal(TreeNode root) {
+//        if (root != null) {
+//            output.add(root.val);
+//            preorderTraversal(root.left);
+//            preorderTraversal(root.right);
+//        }
+//        return output;
+//    }
     public List<Integer> preorderTraversal(TreeNode root) {
-        if (root != null) {
-            output.add(root.val);
-            preorderTraversal(root.left);
-            preorderTraversal(root.right);
+//        if (root == null) return output;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            output.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
         }
         return output;
     }
