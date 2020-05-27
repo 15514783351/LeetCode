7dc3d8132d2a6f3345e073437cf8819a7b934c18
unknown <940929347@qq.com>
2020-05-27 19:07:40 +0800

    杨辉三角

--- src/LeetCode0118.java
+++ src/LeetCode0118.java
@@ -4,21 +4,19 @@ import java.util.List;
 public class LeetCode0118 {
     public List<List<Integer>> generate(int numRows) {
         if (numRows == 0) return new ArrayList<>();
-        List<List<Integer>> res = new ArrayList<>();
         List<Integer> pre = new ArrayList<>();
         pre.add(1);
-        res.add(pre);
-        for (int i = 2; i < numRows + 1; i++) {
-            List<Integer> last = new ArrayList<>();
-            last.add(1);
-            for (int j = 1; j < i - 1; j++) {
-                last.add(pre.get(j - 1) + pre.get(j));
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>(pre));
+        for (int i = 1; i < numRows; i++) {
+            pre.clear();
+            pre.add(1);
+            for (int j = 0; j < i - 1; j++) {
+                pre.add(res.get(i - 1).get(j) + res.get(i - 1).get(j + 1));
             }
-            last.add(1);
-            res.add(last);
-            pre = last;
+            pre.add(1);
+            res.add(new ArrayList<>(pre));
         }
-
         return res;
     }
 }
