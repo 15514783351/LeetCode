97d9a8d341beab34484495622dccfef08860755b
unknown <940929347@qq.com>
2020-05-28 20:06:42 +0800

    数组拆分

--- src/LeetCode0561.java
+++ src/LeetCode0561.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Contact :   dzm_1995@163.com
  * <p>
@@ -9,15 +11,16 @@
 // 数组拆分
 public class LeetCode0561 {
     public int arrayPairSum(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length - 1 - i; j++) {
-                if (nums[j] >= nums[j + 1]) {
-                    int temp = nums[j + 1];
-                    nums[j + 1] = nums[j];
-                    nums[j] = temp;
-                }
-            }
-        }
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = 0; j < nums.length - 1 - i; j++) {
+//                if (nums[j] >= nums[j + 1]) {
+//                    int temp = nums[j + 1];
+//                    nums[j + 1] = nums[j];
+//                    nums[j] = temp;
+//                }
+//            }
+//        }
+        Arrays.sort(nums);
         int res = 0;
         for (int i = 0; i < nums.length / 2; i++) {
             res += nums[2 * i];
