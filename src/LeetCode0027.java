b80a77050706b85a0fc0d1b5e9fb4708f88ed216
unknown <940929347@qq.com>
2020-05-29 20:10:08 +0800

    移除元素

---
+++ src/LeetCode0027.java
@@ -0,0 +1,21 @@
+/**
+ * Contact :   dzm_1995@163.com
+ * <p>
+ * Modify Time       Author     Version    Desciption
+ * ------------      -------    --------    -----------
+ * 2020/5/29 20:04   戴宗明       1.0         None
+ */
+
+// 移除元素
+public class LeetCode0027 {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
