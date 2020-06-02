70792a22f6a360f115c76df370edb5bc157807ca
unknown <940929347@qq.com>
2020-06-02 20:44:06 +0800

    环形链表2

--- src/LeetCode0142.java
+++ src/LeetCode0142.java
@@ -3,23 +3,13 @@ import java.util.Map;
 
 public class LeetCode0142 {
     public ListNode detectCycle(ListNode head) {
-        if (head == null || head.next == null) return null;
-//        ListNode slow = head;
-//        ListNode fast  = head;
-//
-//        while(fast != null && fast.next != null) {
-//            slow = slow.next;
-//            fast = fast.next.next;
-//            if (slow == fast) return slow;
-//        }
-//
-//        return null;
-        Map<ListNode, Integer> hashMap = new HashMap<>();
-        ListNode node = head;
-        while (node != null) {
-            if (hashMap.get(node) != null) return node;
-            else if (hashMap.get(node) == null) hashMap.put(node, 1);
-            node = node.next;
+        Map<ListNode, Integer> map = new HashMap<>();
+
+        while (head != null && head.next != null) {
+            if (map.get(head) == null) {
+                map.put(head, 1);
+                head = head.next;
+            } else return head;
         }
         return null;
     }
