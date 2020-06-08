2b5f4f6d129776c25f9e621a2b8df6264424dc29
unknown <940929347@qq.com>
2020-06-08 19:41:21 +0800

    只出现一次的元素

--- src/LeetCode0136.java
+++ src/LeetCode0136.java
@@ -1,9 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class LeetCode0136 {
     public int singleNumber(int[] nums) {
-        int ans = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            ans = ans ^ nums[i];
+//        int ans = nums[0];
+//        for (int i = 1; i < nums.length; i++) {
+//            ans = ans ^ nums[i];
+//        }
+//        return ans;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (Integer num: nums) {
+            if (map.get(num) != null) {
+                map.put(num, 2);
+            } else {
+                map.put(num, 1);
+            }
         }
-        return ans;
+        for (Integer key: map.keySet()) {
+            if (map.get(key) == 1) return key;
+        }
+        return -1;
     }
 }
