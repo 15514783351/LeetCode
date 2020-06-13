ecc7fad577b2059d5bbc127f9e2dece8d223d7ee
unknown <940929347@qq.com>
2020-06-13 16:04:44 +0800

    字母异位词分组

--- out/production/LEETCODE/src/LeetCode0049.java
+++ out/production/LEETCODE/src/LeetCode0049.java
@@ -11,27 +11,20 @@ import java.util.*;
 // 字母异位词分组
 public class LeetCode0049 {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> out = new ArrayList<>();
-        int length = strs.length;
-        List<String> set = new ArrayList<>();
+        HashMap<String, List<String>> hashMap = new HashMap<>();
         for (String str: strs) {
             char[] arrChar = str.toCharArray();
             Arrays.sort(arrChar);
             String sortStr = new String(arrChar);
-            if (!set.contains(sortStr)) set.add(sortStr);
-        }
-//        System.out.println(set.size());
-        for (int i = 0; i < set.size(); i++) {
-            List<String> temp = new ArrayList<>();
-            for (String str: strs) {
-                char[] arrChar = str.toCharArray();
-                Arrays.sort(arrChar);
-                String sortStr = new String(arrChar);
-                if (set.get(i).equals(sortStr)) temp.add(str);
+            if (hashMap.get(sortStr) == null) {
+                hashMap.put(sortStr, new ArrayList<>());
+                hashMap.get(sortStr).add(str);
+            } else {
+                hashMap.get(sortStr).add(str);
             }
-            out.add(new ArrayList<>(temp));
         }
 
-        return out;
+
+        return new ArrayList<>(hashMap.values());
     }
 }
