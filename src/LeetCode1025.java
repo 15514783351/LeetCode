641e196f3d62b0864c01b490cd3135fa25434cb4
unknown <940929347@qq.com>
2020-07-24 20:25:34 +0800

    除数博弈

---
+++ src/LeetCode1025.java
@@ -0,0 +1,14 @@
+/**
+ * Contact :   dzm_1995@163.com
+ * <p>
+ * Modify Time       Author     Version    Desciption
+ * ------------      -------    --------    -----------
+ * 2020/7/24 20:22   戴宗明       1.0         None
+ */
+
+// 除数博弈
+public class LeetCode1025 {
+    public boolean divisorGame(int N) {
+        return N % 2 == 0;
+    }
+}
