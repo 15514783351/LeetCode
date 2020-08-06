1f6e8ed3c2702b5e5e4ce0cfa0c7bd817f3c4f84
xiaoxin <940929347@qq.com>
2020-08-06 22:26:53 +0800

    回文对

---
+++ src/LeetCode0336.java
@@ -0,0 +1,54 @@
+import java.util.*;
+// 回文对
+public class LeetCode0336 {
+    List<String> wordsRev = new ArrayList<String>();
+    Map<String, Integer> indices = new HashMap<String, Integer>();
+
+    public List<List<Integer>> palindromePairs(String[] words) {
+        int n = words.length;
+        for (String word: words) {
+            wordsRev.add(new StringBuffer(word).reverse().toString());
+        }
+        for (int i = 0; i < n; ++i) {
+            indices.put(wordsRev.get(i), i);
+        }
+
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        for (int i = 0; i < n; i++) {
+            String word = words[i];
+            int m = words[i].length();
+            if (m == 0) {
+                continue;
+            }
+            for (int j = 0; j <= m; j++) {
+                if (isPalindrome(word, j, m - 1)) {
+                    int leftId = findWord(word, 0, j - 1);
+                    if (leftId != -1 && leftId != i) {
+                        ret.add(Arrays.asList(i, leftId));
+                    }
+                }
+                if (j != 0 && isPalindrome(word, 0, j - 1)) {
+                    int rightId = findWord(word, j, m - 1);
+                    if (rightId != -1 && rightId != i) {
+                        ret.add(Arrays.asList(rightId, i));
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+
+    public boolean isPalindrome(String s, int left, int right) {
+        int len = right - left + 1;
+        for (int i = 0; i < len / 2; i++) {
+            if (s.charAt(left + i) != s.charAt(right - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int findWord(String s, int left, int right) {
+        return indices.getOrDefault(s.substring(left, right + 1), -1);
+    }
+}
